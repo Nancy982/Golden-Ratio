843b86c425c63abbaa231f2564584f9ba69a1ea3
Nancy982 <57416480+Nancy982@users.noreply.github.com>
2020-06-11 23:02:57 -0700

    Add files via upload

---
+++ GoldenRatio/GoldenRatio/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUserInput = New System.Windows.Forms.Label()
+        Me.txtUserInput = New System.Windows.Forms.TextBox()
+        Me.txtDisplayFibs = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblNumFibs = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnFibs = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUserInput
+        '
+        Me.lblUserInput.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUserInput.Location = New System.Drawing.Point(-1, 35)
+        Me.lblUserInput.Name = "lblUserInput"
+        Me.lblUserInput.Size = New System.Drawing.Size(343, 37)
+        Me.lblUserInput.TabIndex = 4
+        Me.lblUserInput.Text = "How close to the Golden Ratio should we get?"
+        Me.lblUserInput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtUserInput
+        '
+        Me.txtUserInput.Location = New System.Drawing.Point(324, 43)
+        Me.txtUserInput.Name = "txtUserInput"
+        Me.txtUserInput.Size = New System.Drawing.Size(119, 29)
+        Me.txtUserInput.TabIndex = 0
+        Me.txtUserInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtDisplayFibs
+        '
+        Me.txtDisplayFibs.Location = New System.Drawing.Point(30, 95)
+        Me.txtDisplayFibs.Multiline = True
+        Me.txtDisplayFibs.Name = "txtDisplayFibs"
+        Me.txtDisplayFibs.ReadOnly = True
+        Me.txtDisplayFibs.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtDisplayFibs.Size = New System.Drawing.Size(413, 201)
+        Me.txtDisplayFibs.TabIndex = 5
+        Me.txtDisplayFibs.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(20, 311)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 31)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Its required generating"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblNumFibs
+        '
+        Me.lblNumFibs.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNumFibs.Location = New System.Drawing.Point(170, 308)
+        Me.lblNumFibs.Name = "lblNumFibs"
+        Me.lblNumFibs.Size = New System.Drawing.Size(56, 34)
+        Me.lblNumFibs.TabIndex = 7
+        Me.lblNumFibs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(232, 308)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(324, 37)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "fib numbers to get this close to the Golden Ratio."
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'btnFibs
+        '
+        Me.btnFibs.Location = New System.Drawing.Point(84, 360)
+        Me.btnFibs.Name = "btnFibs"
+        Me.btnFibs.Size = New System.Drawing.Size(133, 51)
+        Me.btnFibs.TabIndex = 1
+        Me.btnFibs.Text = "Generate Fib Numbers"
+        Me.btnFibs.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(237, 360)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(67, 51)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(322, 360)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(66, 51)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(553, 426)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnFibs)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblNumFibs)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDisplayFibs)
+        Me.Controls.Add(Me.txtUserInput)
+        Me.Controls.Add(Me.lblUserInput)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "Form1"
+        Me.Text = "Fibonacci numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+   Friend WithEvents lblUserInput As System.Windows.Forms.Label
+   Friend WithEvents txtUserInput As System.Windows.Forms.TextBox
+   Friend WithEvents txtDisplayFibs As System.Windows.Forms.TextBox
+   Friend WithEvents Label1 As System.Windows.Forms.Label
+   Friend WithEvents lblNumFibs As System.Windows.Forms.Label
+   Friend WithEvents Label2 As System.Windows.Forms.Label
+   Friend WithEvents btnFibs As System.Windows.Forms.Button
+   Friend WithEvents btnClear As System.Windows.Forms.Button
+   Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ GoldenRatio/GoldenRatio/Form1.vb
@@ -0,0 +1,87 @@
+﻿'Nancy Medina
+'CS105
+'Spring-2020
+
+'I pledge that I have neither given nor received help from anyone other than the instructor and TA
+'for all program components included here.
+
+Public Class Form1
+
+    Private Sub btnFibs_Click(sender As Object, e As EventArgs) Handles btnFibs.Click
+        'declare variables for user Input, fib1, fib2, fib3, num fibs, dblDiff, dblRatio, and dblGOLDEN_RATIO
+        Const dblGOLDEN_RATIO As Double = 1.6180339887
+        Dim dblUserInput As Double
+        Dim intFib1 As Integer
+        Dim intFib2 As Integer
+        Dim intFib3 As Integer
+        Dim intNumFibs As Integer
+        Dim dblDiff As Double
+        Dim dblRatio As Double
+
+        'declare a String variable to store all fibs gnerated
+        Dim strDisplay As String
+        'declare a Boolean variable to serve as loop termination condition
+        Dim blnFlag As Boolean
+
+        'collect user input from Text property of txtUserInput
+        dblUserInput = CDbl(txtUserInput.Text)
+
+        'initialize variables to prepare for entering the loop
+        'set up the first three fibs; 0,1,1
+        intFib1 = 0
+        intFib2 = 1
+        intFib3 = intFib1 + intFib2
+        'assign 3 to the num fibs variable, since we have 3 fibs before entering the loop
+        intNumFibs = 3
+
+        'divide intFib3 by intFib2 to find the ratio
+        dblRatio = (intFib3 / intFib2)
+        'subtract the golden ratio from the ratio above
+        dblDiff = Math.Abs(dblGOLDEN_RATIO - dblRatio)
+
+        'format the String variable with the info we have now
+        strDisplay = "Fibonacci numbers generated: " & vbCrLf
+        strDisplay &= intFib1 & ", " & intFib2 & ", " & intFib3
+
+        'initialize the Boolean with the correct Boolean expression so it will correctly control the loop
+        blnFlag = (dblDiff >= dblUserInput)
+
+        'impliment loop to generate remaining fibs
+        Do While blnFlag
+            'reassign the three fibs to get the next fib num int intFib3
+            'the new fib1 is the old fib2
+            intFib1 = intFib2
+            'the new fib2 is the old fib3
+            intFib2 = intFib3
+            'the new fib3 is the sum of the new fib1 and the new fib2
+            intFib3 = intFib1 + intFib2
+            'update dblRatio and dblDiff
+            dblRatio = (intFib3 / intFib2)
+            dblDiff = Math.Abs(dblGOLDEN_RATIO - dblRatio)
+            'increment intNumFibs by 1, since we now have one more fib
+            intNumFibs += 1
+            'APPEND the newest fib to the String variable
+            strDisplay &= ", " & intFib3
+            blnFlag = (dblDiff >= dblUserInput)
+        Loop
+        'display all fibs generated in the Text property of txtDisplayFibs
+        txtDisplayFibs.Text = strDisplay
+        'display how many fibs in the Text property of lblNumFibs
+        lblNumFibs.Text = intNumFibs.ToString()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the TextBox controls
+        txtDisplayFibs.Text = String.Empty
+        txtUserInput.Text = String.Empty
+        lblNumFibs.Text = String.Empty
+
+        'Set the focus to the txtUserInput TextBox; this designates the position of the blinking cursor
+        txtUserInput.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+    End Sub
+End Class
